a400aafbc6c8b162df94f0dd1a70cfba821589f2
Cnamm7 <Cna.mm7@gmail.com>
2022-02-22 11:23:37 -0500

    added manual implementation of swing

--- HelloWorldSwing/src/main/java/com/mycompany/helloworldswing/HelloWorld.java
+++ HelloWorldSwing/src/main/java/com/mycompany/helloworldswing/HelloWorld.java
@@ -3,14 +3,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.helloworldswing;
-
+import javax.swing.*;
 /**
  *
  * @author Cna
  */
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld extends JFrame{
+    JButton btn;
+    JLabel lbl;
+    JPanel panel;
+    public HelloWorld(){        
+        btn = new JButton();
+        lbl = new JLabel();
+        btn.setText("Click Me!");
+        this.setTitle("Hello World from Swing");
+        panel = new JPanel();
+        btn.setToolTipText("Click the button");
         
+        this.setSize(500,300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        
+        panel.add(btn);
+        panel.add(lbl);
+        add(panel);
+        
+        btn.addMouseListener(new java.awt.event.MouseAdapter(){
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent ae) {
+                btnMouseClicked(ae);
+            }
+        });
+    }
+    private void btnMouseClicked(java.awt.event.MouseEvent ae)
+    {
+        lbl.setText("Hello World");
+        
+    }
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable(){
+            public void run(){
+                new HelloWorld();
+            }
+            
+        });
     }
             
             
